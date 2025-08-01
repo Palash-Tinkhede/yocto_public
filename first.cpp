11226bd26b76534d61fa555075ab3d15d0fbfbf3
Palash-Tinkhede <palash.tinkhede@gmail.com>
2025-08-01 10:58:29 +0530

    first commit for test

--- first.cpp
+++ first.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 int main() {
     // Write C++ code here
-    cout <<endl<<" fffffffffffffffffffffff"<<endl;
+    cout <<endl<<" this is git file first commit"<<endl;
 
 
     return 0;
