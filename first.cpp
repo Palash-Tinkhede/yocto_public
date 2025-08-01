508f1228a0cc2372bf73a80f2c76c1e120d4af5d
Palash-Tinkhede <palash.tinkhede@gmail.com>
2025-08-01 10:54:10 +0530

    Add files via upload

---
+++ first.cpp
@@ -0,0 +1,11 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+
+using namespace std;
+int main() {
+    // Write C++ code here
+    cout <<endl<<" fffffffffffffffffffffff"<<endl;
+
+
+    return 0;
+}
